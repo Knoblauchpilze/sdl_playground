201cbfb6e159761b876cdf75bb23bd00ad7db3b4
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-03-01 22:49:11 +0100

    Updated usage of Sizef

--- main.cpp
+++ main.cpp
@@ -32,7 +32,8 @@ int main(int argc, char* argv[]) {
     // Root widget
     sdl::core::SdlWidgetShPtr widget = std::make_shared<sdl::core::SdlWidget>(
       std::string("root_widget"),
-      sdl::core::Boxf(320.0f, 240.0f, 600.0f, 440.0f),
+      //sdl::core::Sizef(320.0f, 240.0f, 600.0f, 440.0f),
+      sdl::utils::Sizef(600.0f, 440.0f),
       nullptr,
       false,
       sdl::core::Palette::fromBackgroundColor(sdl::core::Color(255, 0, 0, SDL_ALPHA_OPAQUE))
@@ -56,7 +57,7 @@ int main(int argc, char* argv[]) {
     // Left widget
     // sdl::core::SdlWidget* widget2 = new sdl::core::SdlWidget(
     //   std::string("left_widget"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget.get(),
     //   false,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(0, 255, 0, SDL_ALPHA_OPAQUE / 2))
@@ -78,43 +79,43 @@ int main(int argc, char* argv[]) {
       widget2
     ));
 
-    sdl::core::SdlWidget* widget21 = new sdl::core::SdlWidget(
-      std::string("widget21"),
-      sdl::core::Boxf(),
-      widget2,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
-    );
-    sdl::core::SdlWidget* widget22 = new sdl::core::SdlWidget(
-      std::string("widget22"),
-      sdl::core::Boxf(),
-      widget2,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 128, SDL_ALPHA_OPAQUE / 2))
-    );
-    sdl::core::SizePolicy policy;
-    policy.setVerticalStretch(2.0f);
-    widget22->setSizePolicy(policy);
+    // sdl::core::SdlWidget* widget21 = new sdl::core::SdlWidget(
+    //   std::string("widget21"),
+    //   sdl::core::Sizef(),
+    //   widget2,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
+    // );
+    // sdl::core::SdlWidget* widget22 = new sdl::core::SdlWidget(
+    //   std::string("widget22"),
+    //   sdl::core::Sizef(),
+    //   widget2,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 128, SDL_ALPHA_OPAQUE / 2))
+    // );
+    // sdl::core::SizePolicy policy;
+    // policy.setVerticalStretch(2.0f);
+    // widget22->setSizePolicy(policy);
 
-    sdl::core::SdlWidget* widget23 = new sdl::core::SdlWidget(
-      std::string("widget23"),
-      sdl::core::Boxf(),
-      widget2,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 128, 64, SDL_ALPHA_TRANSPARENT))
-    );
-    sdl::core::SdlWidget* widget24 = new sdl::core::SdlWidget(
-      std::string("widget24"),
-      sdl::core::Boxf(),
-      widget2,
-      true,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(128, 64, 64, SDL_ALPHA_OPAQUE))
-    );
+    // sdl::core::SdlWidget* widget23 = new sdl::core::SdlWidget(
+    //   std::string("widget23"),
+    //   sdl::core::Sizef(),
+    //   widget2,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 128, 64, SDL_ALPHA_TRANSPARENT))
+    // );
+    // sdl::core::SdlWidget* widget24 = new sdl::core::SdlWidget(
+    //   std::string("widget24"),
+    //   sdl::core::Sizef(),
+    //   widget2,
+    //   true,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(128, 64, 64, SDL_ALPHA_OPAQUE))
+    // );
 
     // Right widget
     // sdl::core::SdlWidget* widget3 = new sdl::core::SdlWidget(
     //   std::string("right_widget"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget.get(),
     //   false,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(0, 0, 255, SDL_ALPHA_OPAQUE))
@@ -152,28 +153,28 @@ int main(int argc, char* argv[]) {
 
     // sdl::core::SdlWidget* widget31 = new sdl::core::SdlWidget(
     //   std::string("widget31"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget3,
     //   false,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
     // );
     // sdl::core::SdlWidget* widget32 = new sdl::core::SdlWidget(
     //   std::string("widget32"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget3,
     //   false,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE / 2))
     // );
     // sdl::core::SdlWidget* widget33 = new sdl::core::SdlWidget(
     //   std::string("widget33"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget3,
     //   false,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_TRANSPARENT))
     // );
     // sdl::core::SdlWidget* widget34 = new sdl::core::SdlWidget(
     //   std::string("widget34"),
-    //   sdl::core::Boxf(),
+    //   sdl::core::Sizef(),
     //   widget3,
     //   true,
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
@@ -212,62 +213,62 @@ int main(int argc, char* argv[]) {
     //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
     // );
 
-    sdl::graphic::LabelWidget* widget31 = new sdl::graphic::LabelWidget(
-      std::string("widget31"),
-      std::string("This is some text"),
-      sdl::core::FontFactory::getInstance().createColoredFont(
-        std::string("data/fonts/times.ttf"),
-        sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
-        20
-      ),
-      sdl::graphic::LabelWidget::HorizontalAlignment::Center,
-      sdl::graphic::LabelWidget::VerticalAlignment::Center,
-      widget3,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
-    );
-    sdl::graphic::LabelWidget* widget32 = new sdl::graphic::LabelWidget(
-      std::string("widget32"),
-      std::string("This is some text"),
-      sdl::core::FontFactory::getInstance().createColoredFont(
-        std::string("data/fonts/times.ttf"),
-        sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
-        20
-      ),
-      sdl::graphic::LabelWidget::HorizontalAlignment::Center,
-      sdl::graphic::LabelWidget::VerticalAlignment::Center,
-      widget3,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE / 2))
-    );
-    sdl::graphic::LabelWidget* widget33 = new sdl::graphic::LabelWidget(
-      std::string("widget33"),
-      std::string("This is some text"),
-      sdl::core::FontFactory::getInstance().createColoredFont(
-        std::string("data/fonts/times.ttf"),
-        sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
-        20
-      ),
-      sdl::graphic::LabelWidget::HorizontalAlignment::Center,
-      sdl::graphic::LabelWidget::VerticalAlignment::Center,
-      widget3,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_TRANSPARENT))
-    );
-    sdl::graphic::LabelWidget* widget34 = new sdl::graphic::LabelWidget(
-      std::string("widget34"),
-      std::string("This is some text"),
-      sdl::core::FontFactory::getInstance().createColoredFont(
-        std::string("data/fonts/times.ttf"),
-        sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE / 2),
-        20
-      ),
-      sdl::graphic::LabelWidget::HorizontalAlignment::Center,
-      sdl::graphic::LabelWidget::VerticalAlignment::Center,
-      widget3,
-      false,
-      sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
-    );
+    // sdl::graphic::LabelWidget* widget31 = new sdl::graphic::LabelWidget(
+    //   std::string("widget31"),
+    //   std::string("This is some text"),
+    //   sdl::core::FontFactory::getInstance().createColoredFont(
+    //     std::string("data/fonts/times.ttf"),
+    //     sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
+    //     20
+    //   ),
+    //   sdl::graphic::LabelWidget::HorizontalAlignment::Center,
+    //   sdl::graphic::LabelWidget::VerticalAlignment::Center,
+    //   widget3,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
+    // );
+    // sdl::graphic::LabelWidget* widget32 = new sdl::graphic::LabelWidget(
+    //   std::string("widget32"),
+    //   std::string("This is some text"),
+    //   sdl::core::FontFactory::getInstance().createColoredFont(
+    //     std::string("data/fonts/times.ttf"),
+    //     sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
+    //     20
+    //   ),
+    //   sdl::graphic::LabelWidget::HorizontalAlignment::Center,
+    //   sdl::graphic::LabelWidget::VerticalAlignment::Center,
+    //   widget3,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE / 2))
+    // );
+    // sdl::graphic::LabelWidget* widget33 = new sdl::graphic::LabelWidget(
+    //   std::string("widget33"),
+    //   std::string("This is some text"),
+    //   sdl::core::FontFactory::getInstance().createColoredFont(
+    //     std::string("data/fonts/times.ttf"),
+    //     sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE),
+    //     20
+    //   ),
+    //   sdl::graphic::LabelWidget::HorizontalAlignment::Center,
+    //   sdl::graphic::LabelWidget::VerticalAlignment::Center,
+    //   widget3,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_TRANSPARENT))
+    // );
+    // sdl::graphic::LabelWidget* widget34 = new sdl::graphic::LabelWidget(
+    //   std::string("widget34"),
+    //   std::string("This is some text"),
+    //   sdl::core::FontFactory::getInstance().createColoredFont(
+    //     std::string("data/fonts/times.ttf"),
+    //     sdl::core::Color(0, 0, 0, SDL_ALPHA_OPAQUE / 2),
+    //     20
+    //   ),
+    //   sdl::graphic::LabelWidget::HorizontalAlignment::Center,
+    //   sdl::graphic::LabelWidget::VerticalAlignment::Center,
+    //   widget3,
+    //   false,
+    //   sdl::core::Palette::fromBackgroundColor(sdl::core::Color(64, 64, 64, SDL_ALPHA_OPAQUE))
+    // );
 
     // Setup application
     app->addWidget(widget);

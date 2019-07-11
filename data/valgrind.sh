aa4ef20c7c9c605d83662249962d830bf2112051
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-07-11 22:56:07 +0200

    Updated valgrind script to provide more insights

--- data/valgrind.sh
+++ data/valgrind.sh
@@ -2,4 +2,4 @@
 
 CURR_DIR=$(dirname $0)
 echo $(cat data/config/local)
-valgrind --leak-check=yes ./bin/sdl_playground $(cat data/config/local)
+valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all ./bin/sdl_playground $(cat data/config/local)

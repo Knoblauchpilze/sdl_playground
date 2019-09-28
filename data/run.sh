becba56ef1ace564ea3e2edb0ce16b0b2d30c006
KnoblauchPC <maxime.rigaud.perso@gmail.com>
2019-09-28 18:04:05 +0200

    Removed debug log in execution scripts

--- data/run.sh
+++ data/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 CURR_DIR=$(dirname $0)
-echo $(cat data/config/local)
 ./bin/sdl_playground $(cat data/config/local)
--- data/valgrind.sh
+++ data/valgrind.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 CURR_DIR=$(dirname $0)
-echo $(cat data/config/local)
 valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all ./bin/sdl_playground $(cat data/config/local)
